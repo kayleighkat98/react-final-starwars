c3fa82c12d1818ff58fe24ea4bc26a20689faa81
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-07-31 15:31:20 -0500

    setup

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+//import './';
+//import  from './';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    };
+  }
+
+  render() {
+    return(
+      <div className='container'>
+         
+      </div>
+    );
+  }
+}
+
+export default App
